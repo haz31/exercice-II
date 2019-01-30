ea3dcd9a482a39e3ac4ee0bbb94e55f74dcbac9f
blaizotsebastien <Blaizotsebastien@yahoo.fr>
2019-01-30 11:46:26 +0100

    exercice II

---
+++ operateurs_conditions.js
@@ -0,0 +1,48 @@
+
+/* Condition 1 */
+
+var a = "michel";
+var b = "drucker";
+if ("michel"=="drucker"){
+	console.log("les valeurs sont égaux")
+  
+}
+else{
+console.log("les valeurs sont différente")
+}
+
+
+/* Condition 2 */
+
+
+ 
+if (42!==42){
+
+     		 console.log(" les valeurs sont inégaux");
+     
+     	}
+     	else if (typeof 42 !== typeof 42) {
+
+       console.log(" types de donnée sont inégaux ");
+       }
+
+else { console.log (" les valeurs et leurs types sont égaux")
+
+ }
+
+/* Condition 3 */
+
+    
+
+if (2===2){
+
+         console.log(" les valeurs et leur types de donnée sont égaux ");
+     
+      }
+      
+
+else { console.log ("les valeurs ou leur types de donnée sont différente")
+
+ }
+
+/* Condition 4 */
---
+++ variable.js
@@ -0,0 +1,7 @@
+var chaine ="characters"
+var chiffre = 40
+Boolean(10 > 9)        // returns true
+var fruits = ['Apple', 'Banane','4','8'];
+
+console.log(fruits.length);      
+console.log(fruits) 
